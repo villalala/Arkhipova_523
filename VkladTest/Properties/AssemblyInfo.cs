13ccd5fcb69132e401c8f46767304e6413e141f0
Архипова Василиса <vasyahi117@gmail.com>
2026-04-02 14:19:13 +0300

    тесты готовы надеюсь

---
+++ VkladTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VkladTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VkladTest")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fa5fda0-c672-4880-a6bd-8b52ef5afd5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VkladTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Arkhipova_523;
+
+namespace VkladTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Simple_Correct()
+        {
+            var page = new Page1();
+            double result = page.CalculateIncome(100000, 12, 12, true);
+            Assert.AreEqual(12000, result, 0.01, "Простые проценты рассчитаны неверно");
+        }
+
+        [TestMethod]
+        public void Compound_Correct()
+        {
+            var page = new Page1();
+            double result = page.CalculateIncome(100000, 12, 12, false);
+            Assert.AreEqual(12682.5, result, 0.01, "Сложные проценты рассчитаны неверно");
+        }
+
+        [TestMethod]
+        public void NegativeSum_Test()
+        {
+            var page = new Page1();
+            double result = page.CalculateIncome(-50000, 12, 10, true);
+            Assert.AreEqual(-5000, result, 0.01, "При отрицательной сумме доход должен быть отрицательным");
+        }
+
+        [TestMethod]
+        public void ZeroMonths_Test()
+        {
+            var page = new Page1();
+            double result = page.CalculateIncome(100000, 0, 12, true);
+            Assert.AreEqual(0, result, 0.01);
+        }
+
+        [TestMethod]
+        public void ZeroProc_Test()
+        {
+            var page = new Page1();
+            double result = page.CalculateIncome(100000, 12, 0, true);
+            Assert.AreEqual(0, result, 0.01);
+        }
+    }
+}
+
